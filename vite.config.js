f3ea437906ccd21d29478f9797c192ca44373a81
Michelle <mpinheiromarcondes@gmail.com>
2025-09-18 05:53:05 -0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Alterado para desenvolvimento local
+  base: '/portfoliopedro', // Alterado para desenvolvimento local
   server: {
     port: 5173,
     open: true
